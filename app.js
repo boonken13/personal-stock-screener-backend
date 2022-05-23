0d71f62165f99b6905ec5c928a60c73b1b516f73
boonken <waynebkken@gmail.com>
2022-05-24 00:04:35 +0800

    test pipeline

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.post('/signin', (req, res) => {signin.signinAuthentication(req, res, bcrypt)
 app.post('/register', (req, res) => { register.handleRegister(req, res, bcrypt) })
 app.get('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileGet(req, res)})
 app.post('/profile/:id', auth.requireAuth, (req, res) => { profile.handleProfileUpdate(req, res)})
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.status(200).send("Hello World!");
 });
 
